4c7455ec886c32a9bcb9efd26f94d367dbbcb854
jake-anderson-r <124278618+jake-anderson-rbe@users.noreply.github.com>
2023-03-23 01:05:21 +0000

    campbell rpg - v.0.0.2 - 2023/03/22
    # changed
    - turned map into database using tile system
    - changed text when you can't go in a direction
    
    # added
    - added inventory system
    - added new items
    - added "search" function

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
-# Jake Anderson
-# CS 30
-# Array Practice
+###############################################################################
+# Title: Campbell RPG      
+# Class: CS 30
+# Date: March 22, 2023
+# Coders Name: Jake Anderson
+# Version: 002   
+###############################################################################
+'''
+Current Assignment: Create a text-based RPG
 
-# functions
+An RPG set within Campbell
+'''
+#------------------------------------------------------------------------------
+#----Imports and Global Variables----------------------------------------------
+import sys
+import random
+#-----Functions----------------------------------------------------------------
+tile = ["Entrance", "Classroom", "Cafeteria", "DeadEnd", "SchoolExit"]
+tiles = {
+    tile[0] : {"description" : "you are at the entrance"},
+    tile[1] : {"description" : "you are in a classroom"},
+    tile[2] : {"description" : "you are in the cafeteria"},
+    tile[3] : {"description" : "there is nothing here"},
+    tile[4] : {"description" : "you found the exit! congrats!"}  
+}
+
 school_map = [
-     ["DeadEnd", "Cafeteria", "Classroom", "DeadEnd"],
-     ["Classroom", "Classroom", "Classroom", "Classroom"],
-     ["DeadEnd", "Entrance", "DeadEnd", "SchoolExit"],
-     ["SchoolExit", "Classroom", "Classroom", "Classroom"]
- ]
+  [tile[3], tile[2], tile[1], tile[3]],
+  [tile[1], tile[1], tile[1], tile[1]],
+  [tile[3], tile[0], tile[3], tile[4]],
+  [tile[4], tile[1], tile[1], tile[1]]
+]
+
+item = ["Pencil", "Textbook", "Calculator", "Binder", "Phone"]
+
+items = {
+  item[0] : {"description": "a pencil you can write with"},
+  item[1] : {"description": "a textbook full of knowledge"},
+  item[2] : {"description": "a calculator for math"},
+  item[3] : {"description": "a binder to put things in"},
+  item[4] : {"description": "a phone that you can use"}
+  
+         }
+
 row = 2 
+
 col = 1
+
 actions = ["walk"]
+
 movements = ["north", "south", "east", "west"]
 
-# defining movement function
+inventory = []
+
+#----Movement Function----------------------------------------------------------------------
 def movement():
   global row, col
   while True:
@@ -37,25 +75,50 @@ def movement():
       print("that's not valid!")
     
   
-# tile functions
+#----Tile Functions----------------------------------------------------------------------
 while True:
   current_location = school_map[row][col]
-  if current_location == "Classroom":
+  if current_location == tile[1]:
     print("you are in a classroom")
-  elif current_location == "Entrance":
+    print("you can search this room!")
+  elif current_location == tile[0]:
     print("you are at the entrance of campbell")
-  elif current_location == "DeadEnd":
+  elif current_location == tile[3]:
     print("there is nothing here")
-  elif current_location == "Cafeteria":
+  elif current_location == tile[2]:
     print("you are in the cafeteria, and eat some food")
-  elif current_location == "SchoolExit":
+  elif current_location == tile[4]:
     print("congrats, you escaped campbell!")
+    sys.exit
   else:
-    print("Somthing went wrong!")
+    print("you can't do that!")
 
-  # action function
+#----Action Functions----------------------------------------------------------------------
   action_input = input("what do you do? ")
   if action_input.lower() == "walk":
     print("you can go:")
     movement()
-
+  elif action_input.lower() == "inventory":
+    print("you currently have:")
+    for item in inventory:
+      print(f"{item}")
+  elif action_input.lower() == "search":
+    if current_location == tile[1]:
+        item_chance = random.randint(0,4)
+        if item_chance == 0:
+          print("you found a pencil!")
+          inventory.append(item[0])
+        elif item_chance == 1:
+          print("you found a textbook!")
+          inventory.append(item[1])
+        elif item_chance == 2:
+          print("you found a calculator!")
+          inventory.append(item[2])
+        elif item_chance == 3:
+          print("you found a binder!")
+          inventory.append(item[3])
+        elif item_chance == 4:
+          print("you found a phone!")
+          inventory.append(item[4])
+    elif current_location != tile[1]:
+      print("you can't do that!")
